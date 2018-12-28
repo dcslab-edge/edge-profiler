5da33372e26a3064525fdfb8f50cc3b7897924d4
YoonsungNam <ysnam@dcslab.snu.ac.kr>
2018-12-28 10:44:22 +0000

    template fix : interval from 1000 to 200

--- benchmark/driver/sparkgpu_data_receiver_python.py
+++ benchmark/driver/sparkgpu_data_receiver_python.py
@@ -32,14 +32,15 @@ class SparkGPUDataReceiverPythonDriver(BenchDriver):
     async def _launch_bench(self) -> asyncio.subprocess.Process:
         data_stream_python_home = BenchDriver.get_bench_home('data-stream-python')
         signal_invoker_home = data_stream_python_home+'/signal-invoker'
-        cmd = '{0}/bin/spark-submit {1}/receiver/sparkgpu_receiver_code.py'.format(self._bench_home, data_stream_python_home)
+        #cmd = '{0}/bin/spark-submit {1}/receiver/sparkgpu_receiver_code.py'.format(self._bench_home, data_stream_python_home)
+        cmd = '{0}/bin/spark-submit {1}/receiver/SPReceive.py'.format(self._bench_home, data_stream_python_home)
 
         env = os.environ.copy()
         env['M2_HOME'] = '/home/nvidia/.m2'
         env['MAVEN_HOME'] = '/home/nvidia/dcslab/packages/apache-maven-3.6.0'
         env['PATH'] = env['MAVEN_HOME'] + '/bin/:' + env['PATH']
 
-        proc=Popen(['python3.7',signal_invoker_home+'/signal_invoker.py',BenchDriver.get_bench_home('local-ip-address')])
+        proc=Popen(['python3.7',signal_invoker_home+'/signal_invoker.py',BenchDriver.get_bench_home('stream-ip-address')])
         
         self.sparkGPU_launched_time = time.time()
         pid = await self._cgroup.exec_command(cmd, env=env,
