d03dab27b92661a216ed8989e2b5cdc67e85138f
YoonsungNam <ysnam@dcslab.snu.ac.kr>
2019-10-11 13:25:04 +0000

    fix: cleanup legacy code in tail_netword_driver.py

--- benchmark/driver/tail_networkd_driver.py
+++ benchmark/driver/tail_networkd_driver.py
@@ -114,47 +114,6 @@ class NTailDriver(BenchDriver):
         bench_output_logger.info(recvd)
         return ended
 
-    async def process_bench_output_temp(self, bench_output_logger: logging.Logger) -> bool:
-        ignore_flag = False
-        """
-        bench_output_logger.info(f'self._bench_driver.is_running: {self._bench_driver.is_running}')
-        bench_output_logger.info(f'self._bench_driver.async_proc.returncode: '
-                                 f'{self._bench_driver.async_proc.returncode}')
-        bench_output_logger.info(f'make_output: '
-                                 f'{make_output}')
-        """
-
-        outdata = await self.waitStreams(NTailDriver.channel)
-        outdata = outdata.replace("\r", '') # removing ^M in the text
-        # bench_output_logger.info(f' outdata: {outdata}')   # output is very long line!
-        if "queue times" or "latencies" in outdata:
-            if "latencies:" in outdata or "times" in outdata:
-                outdata = outdata.replace("latencies: ", '')
-                bench_output_logger.info(outdata)
-            if "end of tail bench" in outdata:
-                outdata = outdata.replace("latencies: ", '')
-                outdata = outdata.replace("end of tail bench\n", "")
-                outdata = outdata.replace("(base) dcslab@bc5:~$", "")    # NOTE: DO NOT FIX!
-                outdata = outdata.rstrip(' \n')
-                bench_output_logger.info(outdata)
-                NTailDriver.channel.close()
-                return True
-        return False
-
-    async def waitStreams_temp(self, chan):
-        #time.sleep(1)
-        outdata = errdata = ""
-        #print(f'[waitStreams] chan.recv_read(): {chan.recv_ready()}')
-        while chan.recv_ready():
-            recvd = chan.recv(4096).decode()
-            #print(f'[waitStreams] recvd: {recvd}')
-            outdata += recvd
-            #outdata += chan.recv(4096).decode()
-        #while chan.recv_stderr_ready():
-        #    errdata += chan.recv_stderr(4096).decode()
-
-        return outdata#, errdata
-
     async def _start_server(self) -> asyncio.subprocess.Process:
         wl_name = NTailDriver.workload_name  # ex) img-dnn
 
@@ -165,76 +124,6 @@ class NTailDriver(BenchDriver):
         print(f'[_start_server] server_exec_cmd: {server_exec_cmd}')
         return await self._cgroup.exec_command(server_exec_cmd, stdout=asyncio.subprocess.PIPE)
 
-    # async def _start_client(self):
-    #     wl_name = NTailDriver.workload_name
-    #     ssh = paramiko.SSHClient()
-    #     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #     ssh.connect('147.46.240.226', username='dcslab')
-    #
-    #     # FIXME: hard-coded for img-dnn (e.g., TBENCH_MNIST_DIR)
-    #     client_env_args = {
-    #         'TBENCH_CLIENT_THREADS': '1',
-    #         'TBENCH_SERVER': '147.46.242.201',
-    #         'TBENCH_SERVER_PORT': str(NTailDriver.PORT_MAPS[wl_name]),
-    #         'TBENCH_QPS': str(NTailDriver.QPS[wl_name]),
-    #         'TBENCH_MINSLEEPNS': '0',
-    #         'TBENCH_RANDSEED': '0',
-    #         'TBENCH_MNIST_DIR': str(NTailDriver.client_input_path)+'/'+str(NTailDriver.CLIENT_DATA_MAPS[wl_name])
-    #     }
-    #     # NOTE: Do not make below cmd as background (DO NOT ATTACH "&" to end of client_bench_cmd)
-    #     client_bench_cmd = f'/ssd2/tailbench/tailbench/{wl_name}/{wl_name}_client_networked'
-    #     print(f'client_bench_cmd: {client_bench_cmd}')
-    #     stdin, stdout, stderr = ssh.exec_command(client_bench_cmd, environment=client_env_args)
-    #
-    #     NTailDriver.client_proc = ssh
-    #     NTailDriver.client_proc_stdout = stdout
-    #     NTailDriver.client_proc_stderr = stderr
-    #     return ssh
-
-    @staticmethod
-    async def _start_client():
-        wl_name = NTailDriver.workload_name
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect('147.46.240.226', username='dcslab')
-
-        # FIXME: hard-coded for img-dnn (e.g., TBENCH_MNIST_DIR)
-        client_env_args = {
-            'TBENCH_CLIENT_THREADS': '1',
-            'TBENCH_SERVER': '147.46.242.201',
-            'TBENCH_SERVER_PORT': str(NTailDriver.PORT_MAPS[wl_name]),
-            'TBENCH_QPS': str(NTailDriver.QPS[wl_name]),
-            'TBENCH_MINSLEEPNS': '0',
-            'TBENCH_RANDSEED': '0',
-            'TBENCH_MNIST_DIR': str(NTailDriver.client_input_path)+'/'+str(NTailDriver.CLIENT_DATA_MAPS[wl_name])
-        }
-        # NOTE: Do not make below cmd as background (DO NOT ATTACH "&" to end of client_bench_cmd)
-        client_bench_cmd = f'/ssd2/tailbench/tailbench/{wl_name}/{wl_name}_client_networked'
-        print(f'client_bench_cmd: {client_bench_cmd}')
-        #stdin, stdout, stderr = ssh.exec_command(client_bench_cmd, environment=client_env_args)
-
-        #NTailDriver.client_proc = ssh
-        #NTailDriver.client_proc_stdout = stdout
-        #NTailDriver.client_proc_stderr = stderr
-        channel = ssh.invoke_shell()
-        #channel.setblocking(0)
-        NTailDriver.channel = channel
-
-        env_line = ""
-        for k, v in client_env_args.items():
-            line = f'{k}={v} '
-            env_line += line
-
-        exec_cmd = env_line + client_bench_cmd + '\n'
-        #channel.send(exec_cmd)
-
-        NTailDriver.stdin = channel.makefile('wb')
-        NTailDriver.stdout = channel.makefile('r')
-        NTailDriver.stdin.write(exec_cmd)
-        NTailDriver.stdin.flush()
-
-        return ssh
-
     @staticmethod
     async def start_async_client() -> None:
         wl_name = NTailDriver.workload_name
@@ -242,7 +131,7 @@ class NTailDriver(BenchDriver):
             print(f'[start_async_client] Trying AsyncSSH connection...')
 
             conn = await asyncssh.connect('147.46.240.226', username='dcslab')
-            #reader, writer = await conn.open_session(command=client_bench_cmd, env=client_env_args)
+
             print(f'[start_async_client] connected! {conn}')
 
             client_env_args = {
@@ -256,17 +145,14 @@ class NTailDriver(BenchDriver):
             }
             client_bench_cmd = f'/ssd2/tailbench/tailbench/{wl_name}/{wl_name}_client_networked'
             exec_cmd = client_bench_cmd + '\n'
-            #test_cmd = f'echo hello asyncssh\n'
-            #channel, session = await conn.open_session(command=exec_cmd, env=client_env_args)
+
             stdin, stdout, stderr = await conn.open_session(env=client_env_args)
-            #print(f'[start_async_client] open_session, ret: {ret}')
+
             print(f'[start_async_client] open_session, stdin: {stdin}')
             print(f'[start_async_client] open_session, stdout: {stdout}')
             print(f'[start_async_client] open_session, stderr: {stderr}')
 
             stdin.write(exec_cmd)
-            #print(f'[start_async_client] open_session, chan: {channel}')
-            #print(f'[start_async_client] open_session, session: {session}')
 
             print(f'[start_async_client] send exec_command, and I\'m alive!! ')
 
@@ -290,10 +176,7 @@ class NTailDriver(BenchDriver):
         print(f'NTailDriver.server_proc.pid: {NTailDriver.server_proc.pid}')
         time.sleep(2)
         # FIXME: If you want to invoke multiple clients, make the below code
-        #NTailDriver.client_proc = await NTailDriver._start_client()
         await NTailDriver.start_async_client()
         print(f'NTailDriver.client_proc: {NTailDriver.reader}')
-        #print(f'NTailDriver.client_proc.pid: {NTailDriver.client_proc.pid}')
 
         return NTailDriver.server_proc
-        # return await self._cgroup.exec_command(cmd, stdout=asyncio.subprocess.DEVNULL)
